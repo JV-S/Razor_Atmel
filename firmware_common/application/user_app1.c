727f2781de0d1b34f01222fa9ad5d09787b4ef9d
JV-S <JV-S@users.noreply.github.com>
2017-11-08 19:50:29 -0700

    led1

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -87,7 +87,15 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
- 
+  LedOff(WHITE);
+  LedOff(PURPLE);
+  LedOff(BLUE);
+  LedOff(CYAN);
+  LedOff(GREEN);
+  LedOff(YELLOW);
+  LedOff(ORANGE);
+  LedOff(RED);
+
   /* If good initialization, set state to Idle */
   if( 1 )
   {
@@ -136,7 +144,55 @@ State Machine Function Definitions
 /* Wait for ??? */
 static void UserApp1SM_Idle(void)
 {
+  static bool bYellowBlink=FALSE;
+  static u16 HertzFactor = 1;
+      if( IsButtonPressed(BUTTON0) )
+    {
+      /* The button is currently pressed, so make sure the LED is on */
+      LedOn(WHITE);
+    }
+    else
+    {
+      /* The button is not pressed, so make sure the LED is off */
+      LedOff(WHITE);
+    }
+    
+     if( WasButtonPressed(BUTTON1) )
+  {
+    /* Be sure to acknowledge the button press */
+    ButtonAcknowledge(BUTTON1);
 
+    /* If the LED is already blinking, toggle it off */
+    if(bYellowBlink)
+    {
+      bYellowBlink = FALSE;
+      LedOff(YELLOW);
+    }
+    else
+    {
+     /* start blinking the LED at the current rate */
+      bYellowBlink = TRUE;
+      LedBlink(YELLOW, (LED_1HZ));
+    }
+  }
+  
+                          if( WasButtonPressed(BUTTON2))
+                          {
+                            ButtonAcknowledge(BUTTON2);
+                            HertzFactor*=2;
+                            if (HertzFactor>8)
+                              HertzFactor=1;
+                            LedBlink(YELLOW, (LED_1HZ/HertzFactor));
+                          }
+  
+        if( IsButtonHeld(BUTTON3, 2000) )
+      {
+        LedOn(CYAN);
+      }
+      else
+      {
+        LedOff(CYAN);
+      }
 } /* end UserApp1SM_Idle() */
     
 
